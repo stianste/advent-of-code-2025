49062a7fe72939c1f2270e43d6a1fa4ff5ebaa14
Stian Steinbakken <stian.steinbakken@posten.no>
2025-12-06 19:54:10 +0100

    [kotlin]: functional solution to day 1.1

--- day01/kotlin/Main.kts
+++ day01/kotlin/Main.kts
@@ -16,26 +16,26 @@ fun main() {
 
   val part2Result = solvePart2SuperLazyNaive(lines)
   println("Part 2: $part2Result")
+
+  val part1FunctionalResult = solvePart1Functional(lines)
+  println("Part 1 (Functional): $part1FunctionalResult")
 }
 
 fun solvePart1NaiveImperative(lines: List<String>): Int {
   var numberOfHits = 0
   var currentPosition = 50
 
-  lines
-    .map { it.splitAtIndex(1) }
-    .map { Pair(it.first, it.second.toInt()) }
-    .forEach { (direction, value) ->
-      when (direction) {
-        "R" -> currentPosition += value
-        "L" -> currentPosition -= value
-        else -> throw IllegalArgumentException("Unknown direction: $direction")
-      }
-
-      if (currentPosition % 100 == 0) {
-        numberOfHits++
-      }
+  mapAndSplit(lines).forEach { (direction, value) ->
+    when (direction) {
+      "R" -> currentPosition += value
+      "L" -> currentPosition -= value
+      else -> throw IllegalArgumentException("Unknown direction: $direction")
     }
+
+    if (currentPosition % 100 == 0) {
+      numberOfHits++
+    }
+  }
 
   return numberOfHits
 }
@@ -44,34 +44,55 @@ fun solvePart2SuperLazyNaive(lines: List<String>): Int {
   var numberOfHits = 0
   var currentPosition = 50
 
-  lines
-    .map { it.splitAtIndex(1) }
-    .map { Pair(it.first, it.second.toInt()) }
-    .forEach { (direction, value) ->
-      when (direction) {
-        "R" -> {
-          repeat(value) {
-            currentPosition++
-            if (currentPosition % 100 == 0) {
-              numberOfHits++
-            }
+  mapAndSplit(lines).forEach { (direction, value) ->
+    when (direction) {
+      "R" -> {
+        repeat(value) {
+          currentPosition++
+          if (currentPosition % 100 == 0) {
+            numberOfHits++
           }
         }
-        "L" -> {
-          repeat(value) {
-            currentPosition--
-            if (currentPosition % 100 == 0) {
-              numberOfHits++
-            }
-          }
-        }
-        else -> throw IllegalArgumentException("Unknown direction: $direction")
       }
+      "L" -> {
+        repeat(value) {
+          currentPosition--
+          if (currentPosition % 100 == 0) {
+            numberOfHits++
+          }
+        }
+      }
+      else -> throw IllegalArgumentException("Unknown direction: $direction")
     }
+  }
 
   return numberOfHits
 }
 
+fun solvePart1Functional(lines: List<String>): Int =
+  mapAndSplit(lines)
+    .fold(Pair(50, 0)) { (currentPosition, numberOfHits), (direction, value) ->
+      val newPosition =
+        when (direction) {
+          "R" -> currentPosition + value
+          "L" -> currentPosition - value
+          else -> throw IllegalArgumentException("Unknown direction: $direction")
+        }
+
+      val hitsThisMove =
+        if (newPosition % 100 == 0) {
+          1
+        } else {
+          0
+        }
+
+      Pair(newPosition, numberOfHits + hitsThisMove)
+    }
+    .second
+
 fun String.splitAtIndex(index: Int): Pair<String, String> = take(index) to substring(index)
+
+fun mapAndSplit(lines: List<String>) =
+  lines.map { it.splitAtIndex(1) }.map { Pair(it.first, it.second.toInt()) }
 
 main()
